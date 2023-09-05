6c116e0fdac0f36c7ad3a8f792a2fe8680275902
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-05 15:00:36 -0300

    make new structure

--- input/nodes/nodeStructure.js
+++ input/nodes/nodeStructure.js
@@ -1,15 +1,23 @@
 
 
-window.nodeElement = function(text = 'empty', color = 'black', children = [],) {
-    return { text, color, children }
+window.nodeElement = function(text = 'empty', color = 'black', children = [], siblingIndex = 0, indexForParent = 0, structureIndex = 0) {
+    return { text, color, children , index: siblingIndex,indexForParent}
 }
 
 if (localStorage.nodeStructure) {
   window.nodeStructure = localStorage.nodeStructure
 }
 else {
-  window.nodeStructure = nodeElement('firstElement', 'black', [nodeElement('secondElement'),nodeElement('thirdElement')]);
+  let firstNode = nodeElement('firstElement', 'black', [nodeElement('secondElement', 'pink'),nodeElement('thirdElement', 'gray')]);
+
+
+  firstNode.children[0].structureIndex = 1;
+  firstNode.children[1].structureIndex = 1;
+  firstNode.children[1].siblingIndex = 1;
+  
+  window.nodeStructure = [[firstNode],[firstNode.children[0], firstNode.children[1]]];
 }
+
 
 
 
