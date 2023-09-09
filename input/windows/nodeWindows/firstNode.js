7e702cadf77b96d64a7d74007528410f0b7f1d61
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-09 15:44:13 -0300

    add the node elements in the select from change first node window

--- input/windows/nodeWindows/firstNode.js
+++ input/windows/nodeWindows/firstNode.js
@@ -1,27 +1,49 @@
-import { currentWindow, closeWindow, displayWindow,displayWindowError } from './../currentWindow.js';
-import {applyFirstNodeFunc} from './../../nodes/firstNode.js';
+import { currentWindow, closeWindow, displayWindow, displayWindowError } from './../currentWindow.js';
+import { applyFirstNodeFunc } from './../../nodes/firstNode.js';
 
 
-export function generateWindowForFirstNodeButton(){
+
+export function generateWindowForFirstNodeButton() {
 
 
   if (windowProperties.isWindowNotOpen) {
-
     currentWindow.innerHTML = `<button id='closeWindow'>close</button>
     <p>First Node</p>
     <select name="" id="">
-    <option value="">empty</option>
     </select>
-    <button id="applyFirstNodeFunc">apply</button>`
+    <button id="applyFirstNodeFunc">apply</button>`;
 
+    appendNodesFromNodeStructure()
     document.querySelector('#closeWindow').addEventListener('click', closeWindow);
     document.querySelector('#applyFirstNodeFunc').addEventListener('click', applyFirstNodeFunc);
 
     windowProperties.isWindowNotOpen = false;
     displayWindow();
   }
-  else{
-  displayWindowError()
+  else {
+    displayWindowError()
   }
 
+}
+
+function appendNodesFromNodeStructure() {
+let select = document.querySelector('#currentWindow select')
+  for (let div of nodeStructure) {
+    for (let node of div) {
+      // this checks that it's not an empty element
+      if (node.color) {
+        let option = document.createElement('option');
+        option.innerText = node.text;
+        option.value = `${node.indexForStructure}-${node.indexForSibling}`;
+        checkIfItMarchesSelectedNode(option)
+        select.appendChild(option);
+      }
+    }
+  }
+}
+
+function checkIfItMarchesSelectedNode(option){
+if(option.value == window.selectedNode){
+option.selected = true;
+}
 }
