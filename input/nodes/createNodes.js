b6fe16404d10e7ff8954bb28d6dae1e3ea47c8c5
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-09 16:29:31 -0300

    update parent amount of spaces

--- input/nodes/createNodes.js
+++ input/nodes/createNodes.js
@@ -1,34 +1,54 @@
 
-let divIndex; 
+let divIndex;
 let nodeIndex;
 
 
 
-export function addNodeToLeft(){
-alert('addNodeToLeft')
+export function addNodeToLeft() {
+  alert('addNodeToLeft')
 }
 
 
-export function addNodeChild(){
-alert('addNodeToRight')
+export function addNodeChild() {
+  alert('addNodeToRight')
 }
 
 
-export function addNodeToRight(){
-divideSelectedNode()
-pushToNodeStructureArray()
+export function addNodeToRight() {
+  divideSelectedNode()
+  pushToNodeStructureArray()
+  updateFollowingElements()
+}
+
+function divideSelectedNode() {
+  let hyphen = selectedNode.indexOf('-');
+  divIndex = selectedNode.substring(0, hyphen);
+  nodeIndex = selectedNode.substring(hyphen + 1, selectedNode.length);
+  console.log(divIndex);
+  console.log(nodeIndex);
 
 }
 
-function divideSelectedNode(){
-let hyphen = selectedNode.indexOf('-');
-divIndex = selectedNode.substring(0, hyphen);
-nodeIndex = selectedNode.substring(hyphen +1, selectedNode.length);
-console.log(divIndex);
-console.log(nodeIndex);
-
+function pushToNodeStructureArray() {
+  nodeStructure[divIndex].splice(nodeIndex, 0, nodeElement('new Element', 'black', emptyElement(), nodeIndex + 1, nodeStructure[divIndex][nodeIndex].indexForParent, divIndex))
 }
 
-function pushToNodeStructureArray(){
-nodeStructure[nodeIndex].splice(divIndex,0, nodeElement('new Element', 'black',emptyElement(),nodeIndex + 1, nodeStructure[nodeIndex][divIndex].indexForParent, divIndex))
+function updateFollowingElements() {
+  console.log(nodeStructure[divIndex][nodeIndex])
+  let parentElement = nodeStructure[divIndex - 1][nodeStructure[divIndex][nodeIndex].indexForParent];
+  // increase amount of space on the Parent because new element was created
+  parentElement.amountOfSpace = parentElement.amountOfSpace + 1;
+  for (let index in nodeStructure) {
+    for (let node of nodeStructure[index2]) {
+      if (node.indexForSibling > nodeIndex && node.indexForStructure > divIndex) {
+        
+
+      }
+
+    }
+
+
+  }
+
+
 }
