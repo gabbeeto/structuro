2f05d9d9cac3dea2e671d5bd36a6f40ae27750a5
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-15 18:19:11 -0300

    fix bug(element is being placed at the end sometimes)

--- input/nodes/createNodes.js
+++ input/nodes/createNodes.js
@@ -4,7 +4,6 @@ let divIndex;
 let nodeIndex;
 
 
-
 export function addNodeToLeft() {
   alert('addNodeToLeft')
 }
@@ -16,8 +15,13 @@ export function addNodeChild() {
 
 
 export function addNodeToRight() {
+  firstNode.amountOfSpace += 1;
+  divIndex = 0;
+  nodeIndex = 1;
   divideSelectedNode()
   pushToNodeStructureArrayAndPushToParent()
+
+
   updateNodeStructure()
 }
 
@@ -25,27 +29,21 @@ function divideSelectedNode() {
   let hyphen = selectedNode.indexOf('-');
   divIndex = selectedNode.substring(0, hyphen);
   nodeIndex = selectedNode.substring(hyphen + 1, selectedNode.length);
-  // console.log(divIndex);
-  // console.log(nodeIndex);
-
 }
+
 
 function pushToNodeStructureArrayAndPushToParent() {
   let pushedNode = nodeElement('new Element', 'black', [emptyElement()], nodeIndex + 1, nodeStructure[divIndex][nodeIndex].indexForParent, divIndex);
-  // console.log(pushedNode)
 
-  console.log(`nodeIndex:${nodeIndex}`)
-  console.log(`divIndex:${divIndex}`)
   nodeStructure[divIndex].splice(nodeIndex + 1, 0, pushedNode)
 
   pushToParent(JSON.stringify(pushedNode))
 }
 
+
 function pushToParent(pushedNode) {
-  // let child = returnChildrenVariableDependingOnChildNumber(Number(divIndex));
   let child = appendArgument('firstNode','children',Number(divIndex));
-  // console.log(divIndex)
- eval(`${child}.splice(nodeIndex + 1, 0,${pushedNode})`) 
-  // eval(`${child}.push(${pushedNode})`)
+
+ eval(`${child}.splice(${nodeIndex} + 1, 0,${pushedNode})`) 
 }
 
